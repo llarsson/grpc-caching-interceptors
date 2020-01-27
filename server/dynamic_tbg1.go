58006387c9bc4b456982e1b6524d2bea956df25f
Lars Larsson <larsson@cs.umu.se>
2020-01-27 14:59:50 +0100

    Array bounds checking

--- server/dynamic_tbg1.go
+++ server/dynamic_tbg1.go
@@ -4,6 +4,8 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type dynamicTBG1Strategy struct {
@@ -19,12 +21,15 @@ func (strat *dynamicTBG1Strategy) initialize() {
 
 func (strat *dynamicTBG1Strategy) determineInterval(intervals *[]interval, verifications *[]verification, estimations *[]estimation) (time.Duration, error) {
 	// Nyqvist sampling theorem, sample twice as fast as the observed frequency
-	return (*estimations)[len(*estimations)].validity / 2, nil
+	if len(*estimations) > 0 {
+		return (*estimations)[len(*estimations)-1].validity / 2, nil
+	}
+	return -1, status.Errorf(codes.Internal, "Not enough estimates to determine interval")
 }
 
 func (strat *dynamicTBG1Strategy) determineEstimation(intervals *[]interval, verifications *[]verification, estimations *[]estimation) (time.Duration, error) {
 	// Rerteive newest message
-	newMessage := (*verifications)[len(*verifications)]
+	newMessage := (*verifications)[len(*verifications)-1]
 
 	// If there is difference between this and the previous sample, save time stamp
 	if proto.Equal(newMessage.reply, strat.prevMessage.reply) {
@@ -34,7 +39,7 @@ func (strat *dynamicTBG1Strategy) determineEstimation(intervals *[]interval, ver
 	// Run through all timestamps and estimate validity period
 	avgDur := 0.0
 	nbrDelta := len(strat.deltaTimestamps)
-	for i := nbrDelta; i > 0; i-- {
+	for i := nbrDelta - 1; i > 0; i-- {
 		avgDur += (strat.deltaTimestamps[i]).Sub((strat.deltaTimestamps)[i-1]).Seconds() / float64(nbrDelta)
 	}
 
