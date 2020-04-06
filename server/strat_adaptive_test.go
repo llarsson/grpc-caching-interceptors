d546c09c945a7ba26bfe9fe4d9622e90ba063cda
Lars Larsson <larsson@cs.umu.se>
2020-04-06 14:30:07 +0200

    Conservative test case

--- server/strat_adaptive_test.go
+++ server/strat_adaptive_test.go
@@ -46,6 +46,27 @@ func TestAdaptiveWithoutChange(test *testing.T) {
 	}
 }
 
+func TestAdaptiveWithoutChangeConservative(test *testing.T) {
+	var s mockMessage
+
+	s = sample{value: "0"}
+	strat := &adaptiveStrategy{alpha: 0.1}
+	strat.initialize()
+
+	var t time.Time
+	t = time.Now().Add(-10 * time.Second)
+
+	for i := 0; i < 10; i++ {
+		strat.update(t, s.(proto.Message))
+		t = t.Add(1 * time.Second)
+	}
+
+	got := strat.determineEstimation()
+	if int(got.Seconds()) != 1 {
+		test.Errorf("Wanted 1 second TTL, got %v", got)
+	}
+}
+
 func TestAdaptiveWithChange(test *testing.T) {
 	var s mockMessage
 
